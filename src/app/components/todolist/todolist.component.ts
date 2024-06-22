f90e4ebe28ae445c00d502a7ca314f81d270b5b2
Evandro Santos <vandersant7@gmail.com>
2024-06-22 01:40:18 -0300

    feat: store function in localstorage

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -1,38 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+interface TaskModel {
+  taskName: string;
+  isCompleted: boolean;
+}
 
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrl: './todolist.component.scss'
+  styleUrl: './todolist.component.scss',
 })
 
+export class TodolistComponent implements OnInit {
+  taskArray: TaskModel[] = [];
 
-export class TodolistComponent {
-
-
-  taskArray = [{taskName: 'Adicione uma tarefa', isCompleted: false }]
-
-  onSubmit(form: NgForm){
-    this.taskArray.push({
-      taskName: form.controls['task'].value,
-      isCompleted: false
-    })
-
-    localStorage.setItem('', 'taskArray')
-
-    form.reset()
+  ngOnInit(): void {
+    this.GetAll();
   }
 
-  onDelete(index: number){
-    this.taskArray.splice(index, 1)
+  onSubmit(form: NgForm) {
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false,
+    });
 
+    this.Save();
+
+    form.reset();
+  }
+
+  onDelete(index: number) {
+    this.taskArray.splice(index, 1);
+    this.Save();
   }
 
   onChecked(index: number) {
-    console.log(this.taskArray)
+    console.log(this.taskArray);
 
-    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+    this.Save();
   }
 
+  Save() {
+    localStorage.setItem('todo', JSON.stringify(this.taskArray));
+  }
+
+  GetAll() {
+    let value = localStorage.getItem('todo');
+    if (value) {
+      this.taskArray = JSON.parse(value);
+    } else {
+      this.taskArray = [
+        { taskName: 'Adicione uma tarefa', isCompleted: false },
+      ];
+    }
+  }
 }
