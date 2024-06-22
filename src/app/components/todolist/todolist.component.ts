859c8af293adda69b0966a07c7e41f1632f78502
Evandro Santos <vandersant7@gmail.com>
2024-06-22 14:30:10 -0300

    refactor todolist component and todolist template html

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -1,59 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
-interface TaskModel {
-  taskName: string;
-  isCompleted: boolean;
-}
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { TodoServiceService } from '../../services/todo-service.service';
+import { TaskModel } from '../../models/TodoModel';
 
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.scss',
 })
-
 export class TodolistComponent implements OnInit {
-  taskArray: TaskModel[] = [];
+
+  taskForm: FormGroup;
+
+  tasks: TaskModel[] = []
+
+  constructor(private todoService: TodoServiceService) {}
 
   ngOnInit(): void {
-    this.GetAll();
-  }
-
-  onSubmit(form: NgForm) {
-    this.taskArray.push({
-      taskName: form.controls['task'].value,
-      isCompleted: false,
+    this.taskForm = new FormGroup({
+      task: new FormControl('', Validators.required)
     });
 
-    this.Save();
-
-    form.reset();
+    this.todoService.getTasks().subscribe(tasks => {
+      this.tasks = tasks;
+    })
   }
 
-  onDelete(index: number) {
-    this.taskArray.splice(index, 1);
-    this.Save();
-  }
-
-  onChecked(index: number) {
-    console.log(this.taskArray);
-
-    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
-    this.Save();
-  }
-
-  Save() {
-    localStorage.setItem('todo', JSON.stringify(this.taskArray));
-  }
-
-  GetAll() {
-    let value = localStorage.getItem('todo');
-    if (value) {
-      this.taskArray = JSON.parse(value);
-    } else {
-      this.taskArray = [
-        { taskName: 'Adicione uma tarefa', isCompleted: false },
-      ];
+  onSubmit(): void {
+    if (this.taskForm.valid) {
+      this.todoService.addTask(this.taskForm.get('task')?.value);
+      this.taskForm.reset();
     }
   }
+
+  onDelete(index: number): void {
+   this.todoService.deleteTask(index)
+  }
+
+  onChecked(index: number): void {
+    this.todoService.toggleTaskCompletetion(index)
+  }
+
 }
